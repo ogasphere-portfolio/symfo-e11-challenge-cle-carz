ed76e18ea96f5975ef5d28379866813e1e1b450a
Olivier <olivier@ogasphere.com>
2021-09-28 18:28:42 +0200

    Mise en place du bread BrandController

--- src/Controller/BrandController.php
+++ src/Controller/BrandController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-use App\Entity\Car;
+use App\Entity\Brand;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,9 +10,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 
 /**
- * @Route("/admin/car", name="admin_car_")
+ * @Route("/admin/brand", name="admin_brand_")
  */
-class CarController extends AbstractController
+class BrandController extends AbstractController
 {
     
      /**
@@ -21,7 +21,7 @@ class CarController extends AbstractController
     public function browse(Request $request): Response
     {
         // on fournit ce formulaire à notre vue
-        return $this->render('admin/car/browse.html.twig', [
+        return $this->render('admin/brand/browse.html.twig', [
         ]);
     }
 
@@ -30,67 +30,63 @@ class CarController extends AbstractController
      */
     public function add(Request $request): Response
     {
-        $car= new Car();
+        $brand= new Brand();
 
         // on créé un formulaire vierge (sans données initiales car l'objet fournit est vide)
-        $carForm = $this->createForm(CarType::class, $car);
+        $brandForm = $this->createForm(BrandType::class, $brand);
 
         // Après avoir été affiché le handleRequest nous permettra
         // de faire la différence entre un affichage de formulaire (en GET) 
         // et une soumission de formulaire (en POST)
         // Si un formulaire a été soumis, il rempli l'objet fournit lors de la création
-        $carForm->handleRequest($request);
+        $brandForm->handleRequest($request);
 
         // l'objet de formulaire a vérifié si le formulaire a été soumis grace au HandleRequest
         // l'objet de formulaire vérifie si le formulaire est valide (token csrf mais pas que)
-        if ($carForm->isSubmitted() && $carForm->isValid()) {
+        if ($brandForm->isSubmitted() && $brandForm->isValid()) {
 
             // on ne demande l'entityManager que si on en a besoin
             $entityManager = $this->getDoctrine()->getManager();
 
-            $entityManager->persist($car);
+            $entityManager->persist($brand);
             $entityManager->flush();
 
             // pour opquast 
-            $this->addFlash('success', "Le vehicule `{$car->getModel()}` a bien été ajoutée");
+            $this->addFlash('success', "La marque `{$brand->getName()}` a bien été ajoutée");
 
             // redirection
-            return $this->redirectToRoute('admin_car_browse');
+            return $this->redirectToRoute('admin_brand_browse');
         }
 
         // on fournit ce formulaire à notre vue
-        return $this->render('admin/car/add.html.twig', [
-            'car_form' => $carForm->createView(),
+        return $this->render('admin/brand/add.html.twig', [
+            'brand_form' => $brandForm->createView(),
         ]);
     }
 
     /**
      * @Route("/update/{id}", name="update", methods={"GET", "POST"})
      */
-    public function update(Request $request, Car $car): Response
+    public function update(Request $request, Brand $brand): Response
     {
-        $carForm = $this->createForm(CategoryType::class, $car);
+        $brandForm = $this->createForm(CategoryType::class, $brand);
 
-        $carForm->handleRequest($request);
+        $brandForm->handleRequest($request);
 
-        if ($carForm->isSubmitted() && $carForm->isValid()) {
+        if ($brandForm->isSubmitted() && $brandForm->isValid()) {
             $entityManager = $this->getDoctrine()->getManager();
 
             $entityManager->flush();
 
-            $this->addFlash('success', "Le vehicule `{$car->getModel()}` a bien été mise à jour");
+            $this->addFlash('success', "Le vehicule `{$brand->getName()}` a bien été mise à jour");
 
-            return $this->redirectToRoute('admin_car_browse');
+            return $this->redirectToRoute('admin_brandForm_browse');
         }
 
         // on fournit ce formulaire à notre vue
-        return $this->render('admin/car/add.html.twig', [
-            'category_form' => $carForm->createView(),
+        return $this->render('admin/brandForm/add.html.twig', [
+            'category_form' => $brandForm->createView(),
         ]);
     }
 
 }
-
-
-
-   
